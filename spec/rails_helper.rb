bf99ebdc7f882293f542557de3be522b6bafce2d
Scott Matthewman <scott@matthewman.net>
2023-04-30 20:00:27 +0100

    Set up RSpec, Capybara, WebMock and RuboCop

---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
+require 'webmock/rspec'
+
+# Automatically load all helpers, defined in spec/initializers/*.rb
+# Dir[Rails.root.join('spec/initializers/**/*.rb')].each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
