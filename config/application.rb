32b78c653be5bedd6fb450dc5c602d8f736e6722
Scott Matthewman <scott@matthewman.net>
2023-05-01 16:49:53 +0100

    Reintroduce Turbo and Stimulus
    This puts back some of the features originally omitted by use of the
    `--minimal` flag when generating the initial Rails app

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ require 'action_controller/railtie'
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require 'action_view/railtie'
-# require "action_cable/engine"
-# require "rails/test_unit/railtie"
+require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
