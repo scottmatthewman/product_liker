8f2146b21a69881cc32d7d9dd86780b667c48493
Scott Matthewman <scott@matthewman.net>
2023-05-01 12:39:56 +0100

    Add more details and style the page
    Each product in the list is given a card, automatically arranged in a
    responsive grid with a slight zoom on mouse hover. Key elements are
    validated in the system specs.
    
    A helper method creates the image for each card, or a placeholder
    view if no image is present.
    
    System specs for the card views verify the key information based on the
    amended test JSON.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,10 @@
 module ProductsHelper
+  def product_image(product, size: :small)
+    img_src = product.dig(:photos, 0, :files, size)
+    if img_src
+      image_tag(img_src, size: '80x80', alt: "User-supplied photo of #{product[:title]}")
+    else
+      content_tag(:div, 'No image', class: 'no-image')
+    end
+  end
 end
